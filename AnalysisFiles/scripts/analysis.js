bdd741f38edcf64f3210d6b45d311de4b4bcd2b3
Mason Secky-Koebel <mseckykoebel@gmail.com>
2019-02-06 22:49:50 -0600

    added the CSV loader...may not need to use but we will see

--- AnalysisFiles/scripts/analysis.js
+++ AnalysisFiles/scripts/analysis.js
@@ -29,27 +29,32 @@ function runAnalysis() {
         console.log(trainingSet.length);
 
         // run for 40 values of K
-        _.range(1, 35).forEach(k => {
+        _.range(1, 40).forEach(k => {
             // Pass in an array and a function
             // testPoint is the value we care about, and this is testSet[0], as it has the label
+            // _.tail(shuffledTestPoint) is shuffledTestSet minus the label in the array
             const accuracy = _.chain(shuffledTestSet)
-                .filter(shuffledTestPoint => knn(shuffledTestSet, _.tail(shuffledTestPoint), k) === shuffledTestPoint[0])
+                .filter(shuffledTestPoint => knn(shuffledTrainingSet, _.tail(shuffledTestPoint), k) === shuffledTestPoint[0])
                 .size()
                 .divide(testSetSize) // only pass in the number we are dividing by
                 .value()
 
             // Now we return an array that only has the accuracy metric
-            console.log('For a K value of ', k, ', the accuracy is ', accuracy * 100, ' percent.');
+            console.log('For a K value of ', k, ', the KNN funtion is ', accuracy * 100, ' percent accurate.');
         })
 
-    } else {
+    } else if (testSetSize === 104) {
         // Check for values of K between 1 - 20
         // data is the trainingSet passed into KNN alg
         // testPoint is one of the arrays in testSet, omitting the first point which is the label
         // This will see how good it predicts the label, which is testPoint[0]
-        _.range(1, 35).forEach(k => {
+        _.range(1, 40).forEach(k => {
             // Pass in an array and a function
             // testPoint is the value we care about, and this is testSet[0], as it has the label
+            // trainingSet has the label and all (as we need it)
+            // pass in the testArray minus the label as we do not need to see it to do analysis
+            // see how it compares to the label
+            // when the guess is the same, increment the value accuracy!!! this is all done here
             const accuracy = _.chain(testSet)
                 .filter(testPoint => knn(trainingSet, _.tail(testPoint), k) === testPoint[0])
                 .size()
@@ -57,8 +62,11 @@ function runAnalysis() {
                 .value()
 
             // Now we return an array that only has the accuracy metric
-            console.log('For a K value of ', k, ', the accuracy is ', accuracy * 100, ' percent.');
+            console.log('For a K value of ', k, ', the KNN funtion is ', accuracy * 100, ' percent accurate.');
         })
+    } else {
+        console.error("Please enter a value between 0 and 105, not including 105");
+        
     }
 
 }
@@ -66,10 +74,10 @@ function runAnalysis() {
 // Function determines the distance between two features, and returns them
 // Distance determined by the pythagorean theorem
 // (arr1, arr2) => float
+// pointA and pointB are both arrays
 function distance(pointA, pointB) {
     return _.chain(pointA)
-        // zip will merge pointA[i] and pointB[i] into one array
-        .zip(pointB)
+        .zip(pointB) // array of arrays, matching the indicies and making an array of 2-element arrays [ [1, 2], [x, y], ... ]
         .map(([a, b]) => (a - b) ** 2)
         .sum()
         .value() ** 0.5;
@@ -83,15 +91,17 @@ function distance(pointA, pointB) {
 function knn(data, point, k) {
     // "point" is the remainder of the features, and compared to the first 
     // _.tail(row) is all but the first array element
+    // data is the entire trainingSet array with the label and all
     return _.chain(data)
         .map(row => {
+            // distance from the
             return [distance(_.tail(row), point),
                 _.head(row)
             ];
         })
         .sortBy(row => row[0]) // sort by least to greatest differences
-        .slice(0, k) // cut the array down by the top "k" records!! 
-        .countBy(row => row[0]) // sort by the most common label, located in row[0]
+        .slice(0, k) // cut the array down by the top "k" records!! GOOD  
+        .countBy(row => row[0]) // sort by the most common label, located in row[0] GOOD
         .toPairs() // convert the above to arrays, and not objects
         .sortBy(row => row[1]) // sort by least->greatest, and most common label will be at the bottom
         .last() // return the last element in the big array [56, -1] for example
--- AnalysisFiles/scripts/load-csv.js
+++ AnalysisFiles/scripts/load-csv.js
@@ -1,1 +1,46 @@
-// Enter the code for loadCSV here
+// THIS IS THE NEW FILE FOR THE CSV CONVERTER
+var $ = require('jquery-browserify')
+
+$("img[attr$='png']").hide()
+
+browserify({
+    require: {
+        jquery: 'jquery-browserify'
+    }
+});
+var something = require('jquery')
+//^^^ Those are just there for reference in this code I believe, that will go elsewhere. 
+
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "Training_Dataset.csv",
+        dataType: "csv",
+        success: function (data) {
+            processData(data);
+        }
+    });
+});
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i = 1; i < allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+
+            for (var j = 0; j < headers.length; j++) {
+                tarr.push(headers[j] + ":" + data[j]);
+            }
+
+            lines.push(tarr);
+        }
+    }
+    // alert(lines);
+}
+
+// I'm 90% sure at this point, lines is an array of all the text.
